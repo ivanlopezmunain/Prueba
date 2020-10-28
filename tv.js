5ca5a8e22ea0b3a958b33935ecfa85efcf59e4f0
ivanlopezmunain <55379549+ivanlopezmunain@users.noreply.github.com>
2020-10-28 10:04:17 +0100

    Update tv.js

--- tv.js
+++ tv.js
@@ -11,7 +11,7 @@
     myConnector.getSchema = function(schemaCallback) {
         var cols = [
             //{ id: "prueba", dataType: tableau.dataTypeEnum.int } ,
-            { id: "popu", dataType: tableau.dataTypeEnum.string }/*,
+            { id: "popu", dataType: tableau.dataTypeEnum.float }/*,
             { id: "id", dataType: tableau.dataTypeEnum.int },
             { id: "backdrop_path", dataType: tableau.dataTypeEnum.string },
             { id: "vote_average", dataType: tableau.dataTypeEnum.float },
@@ -54,7 +54,7 @@
     function getResultsPromise(table, pageNum) {
         return new Promise(function(resolve, reject) {
             var connectionUrl = base_uri + "tv/popular?api_key=" + api_key + "&page=" + pageNum;
-            var connectionUrl1 = "https://emios001.energy-minus.es/src/api/dame_valores_rango_fechas_sensor.php?usuario=redefinetika&contrasenya=3e1738fc5f114ed9234b9e02a2146a16&id_sensor=6415&intervalo_valores=cuartohora&fecha_hora_inicio=01-09-2020_17:00:00&fecha_hora_fin=01-09-2020_18:00:00&id_red=117"
+            //var connectionUrl1 = "https://emios001.energy-minus.es/src/api/dame_valores_rango_fechas_sensor.php?usuario=redefinetika&contrasenya=3e1738fc5f114ed9234b9e02a2146a16&id_sensor=6415&intervalo_valores=cuartohora&fecha_hora_inicio=01-09-2020_17:00:00&fecha_hora_fin=01-09-2020_18:00:00&id_red=117"
             
             let url = connectionUrl1;
             
@@ -77,7 +77,7 @@
                         _.each(data.results, function(record) {               
                             entry = {
                                 //"prueba": record.numero_tuplas_valores ,
-                                "popu": "ajaj"   /*,
+                                "popu": record.popularity   /*,
                                 "id": record.id,
                                 "backdrop_path": images_uri + record.backdrop_path,
                                 "vote_average": record.vote_average,
